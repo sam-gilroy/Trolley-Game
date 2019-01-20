34ba77e0d72a5cbef6daf8f8a6151bc956a10437
Luke Desktop <meistermayo@gmail.com>
2019-01-20 15:42:00 -0500

    CharacterManager Initial push
    (Added some default materials)
    
    Made a bare bones CharacterManager
    all it does is spawn a random character -- currently populates its collection of charcaters from a Resources directory. Will need to be changed to act like ParticleManager.
    
    Populated Resources directory with prefabs

---
+++ Trolley-Game/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameJamTools;
+
+/// <summary>
+/// What are the responsibilities of the character manager?
+/// Does it need stratified pools? ye prolly
+/// 
+/// It needs to have a collection of spawn points
+/// Needs to be able to spawn certain types of characters
+/// 
+/// Probably needs a tiered object pool a la how the ParticleManger does it
+/// </summary>
+public class CharacterManager : Singleton<CharacterManager>, ISingleton {
+    [SerializeField] CharacterComponent DefaultCharacterPrefab;
+
+    CharacterComponent[] CharacterPrefabs;
+
+    public void Init()
+    {
+        CharacterPrefabs = Resources.LoadAll<CharacterComponent>("Prefabs/Characters");
+        SpawnRandomCharacter();
+    }
+
+    public void SpawnRandomCharacter()
+    {
+        int r = Random.Range(0, CharacterPrefabs.Length);
+        Instantiate(CharacterPrefabs[r], transform.position, Quaternion.identity);
+    }
+
+    public void SpawnRandomCharacter(Transform transform)
+    {
+        int r = Random.Range(0, CharacterPrefabs.Length);
+        Instantiate(CharacterPrefabs[r], transform.position, Quaternion.identity);
+    }
+
+}
