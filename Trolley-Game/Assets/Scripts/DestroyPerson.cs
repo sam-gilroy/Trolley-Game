0808f8ac1ffd96d30f3c1a2dc51927fcef16df11
= <sam.gilroy2@gmail.com>
2019-01-25 17:52:45 -0600

    Destroy person

---
+++ Trolley-Game/Assets/Scripts/DestroyPerson.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPerson : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log (col.tag);
+		if (col.CompareTag("Person")) {
+			Destroy (col.gameObject);
+		}
+	}
+}
